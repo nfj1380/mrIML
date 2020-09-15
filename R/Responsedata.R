b94fcefc06ab36bf86132a2a1e0cd2bcbe142cdb
nfj1380 <nfountainjones@gmail.com>
2020-09-15 15:17:09 +1000

    responseData

---
+++ R/Responsedata.R
@@ -0,0 +1,11 @@
+#' Viral single nucleuotide data.
+#'
+#' @source Fountain‐Jones, NM, Craft, ME, Funk, WC, et al. Urban landscapes can change virus gene flow and evolution in a fragmentation‐sensitive carnivore. Mol Ecol. 2017; 26: 6487– 6498. https://doi.org/10.1111/mec.14375
+#' @format A data frame with 171 columns 19 rows:
+#' \describe{
+#'  \item{env1}{Presence/absence data from one loci across 19 bobctas from the envelope gene}
+#' @examples
+#' \dontrun{
+#'  Responsedata
+#' }
+"Responsedata"
