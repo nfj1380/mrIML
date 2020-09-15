25e2449f302760f57a2c6d6515235facef140172
nfj1380 <nfountainjones@gmail.com>
2020-09-15 15:31:01 +1000

    Feature data

---
+++ R/Features.R
@@ -0,0 +1,13 @@
+#' Landscape and host feature (variable) data 
+#'
+#' @source Fountain‐Jones, NM, Craft, ME, Funk, WC, et al. Urban landscapes can change virus gene flow and evolution in a fragmentation‐sensitive carnivore. Mol Ecol. 2017; 26: 6487– 6498. https://doi.org/10.1111/mec.14375
+#' @format A data frame with 20 columns (features) and 19 rows (sampled bobcat locations):
+#' \describe{
+#'  \item{Grassland}{the proportion of grassland in a 5km radius of the sample location}
+#'  \item{Shrub.Scrub}{the proportion of scrub in a 5km radius of the sample location}
+#'   \item{Forest}{the proportion of forest in a 5km radius of the sample location}
+#' @examples
+#' \dontrun{
+#'  Features
+#' }
+"Features"
