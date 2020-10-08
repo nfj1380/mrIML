d0840eb235646fbfb58ca7b1e72811bb0a817213
nfj1380 <nfountainjones@gmail.com>
2020-10-08 11:25:27 +1100

    vignette updates

---
+++ R/gfData.R
@@ -0,0 +1,10 @@
+#' Regression data from Fitzpatrick et al 2014
+#'
+#' @source Fitzpatrick et al 2014 Ecology Letters doi: 10.1111/ele.12376
+#' \describe{
+#'  \item{gfData} Propotion of individuals in each population with each SNP and bioclim data (bio_1, bio_2, bio_7. bio_10, bio_15, bio_18 as well as spatial MEMs)
+#' @examples
+#' \dontrun{
+#'  Responsedata
+#' }
+"gfData"
