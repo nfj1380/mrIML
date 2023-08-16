ec081986a91f1d72cb48cef94cbf24b5169a418d
Nick Fountain-Jones <33707823+nfj1380@users.noreply.github.com>
2023-08-16 11:07:39 +1000

    minor improvements to simulation code
    Pipeline now working

--- Part1_Simulation.R
+++ Part1_Simulation.R
@@ -5,12 +5,11 @@
 # Kazuhiro Takemoto (takemoto@bio.kyutech.ac.jp)
 #####################################################################
 
-library(seqtime) # https://github.com/hallucigenia-sparsa/seqtime
-library(SpiecEasi) # https://github.com/zdk123/SpiecEasi
-library(igraph) # https://igraph.org/r/
-library(ppcor) # https://cran.r-project.org/web/packages/ppcor/index.html
-library(PRROC) # https://cran.r-project.org/package=PRROC
-# load generateM_specific_type function
+pacman::p_load('seqtime','SpiecEasi', 'igraph', 'mrIML', 'ppcor', 'PRROC')
+
+# library(seqtime) # https://github.com/hallucigenia-sparsa/seqtime
+# library(SpiecEasi) # https://github.com/zdk123/SpiecEasi
+#install_github("zdk123/SpiecEasi")
 source("generateM_specific_type.R")
 
 # library(devtools)  
@@ -21,7 +20,7 @@ nn <- 40 # network size (n)
 k_ave <- 2 # average degree (<k>). Could base this off real world nets?
 
 ## Genrate an interaction matrix (Mij)
-obj <- generateM_specific_type(nn,k_ave,type.network="random",type.interact="mix",interact.str.max=0.8,mix.compt.ratio=0.5)
+obj <- generateM_specific_type(nn,k_ave,type.network="random",type.interact="mutual",interact.str.max=0.8,mix.compt.ratio=0.5)
 # @param nn number of nodes
 # @param k_ave average degree (number of edges per node)
 # @param type.network network structure
@@ -47,7 +46,7 @@ M <- obj[[2]]
 Mdf <-as.data.frame( obj[[2]])
 colnames(Mdf) <- row.names(Mdf)
 #convert to igraph
-diag(M) <- 0
+#diag(M) <- 0
 diag(Mdf) <- 0
 g <- graph.adjacency(M, mode = "undirected", weighted = TRUE)#matching Y data
 
@@ -87,14 +86,19 @@ r <- runif(nn)
 res <- glv(nn, M, r, y)
 tsplot(10*res[,20:1000],time.given =T)
 
+#################################################
 ## Generate dataset on species abundance using the GLV model. 300 samples
-data <- as.data.frame(generateDataSet(300, M, count = nn*100, mode = 4)) #mode follows from Hirano and Takemoto 2019
+#################################################
+
+#use defaults
+data <- as.data.frame(generateDataSet(300, M, count = nn*100, mode = 4, maxsteps=)) #mode follows from Hirano and Takemoto 2019
+
 # relative abundance
 data_relative <- as.data.frame(t(data) / apply(data,2,sum))
 str(data_relative)
  data_mean <- colMeans(data_relative )
 
-# Transform the data to meet the condition
+# Transform the data to make presence absence
 data_pa  <- data_relative %>%
   mutate(across(everything(), ~ ifelse(. < data_mean, 0, 1)))# %>%
   #mutate_all(factor)
@@ -104,7 +108,7 @@ data_pa  <- data_relative %>%
 #MrIML analysis
 ##############################################################
 
-Y <- filterRareCommon(data_pa, lower=0.1, higher=0.9)  #9 lost
+Y <- filterRareCommon(data_pa, lower=0.1, higher=0.9)  #9 taxa lost. Mostly had high abundance at each site
 
 #set up interactions
 X1 <- Y 
@@ -149,7 +153,7 @@ pds[[2]]#plot
 assoc_net<- mrCoOccurNet_bootstrap (mrPD_obj=pds , Y=Y)
 
 assoc_net_filtered <-  assoc_net %>% 
-  filter(mean_strength > 0.025) #seems like the sweetspot? worse a 0.1
+  filter(mean_strength > 0.025) #seems like the sweetspot? 
 
 #to make everthing comparable
 transformed_strength <- assoc_net_filtered  %>%
@@ -165,8 +169,7 @@ g <- graph_from_data_frame(assoc_net_filtered, directed=FALSE, vertices=names(Y)
 E(g)$Value <- assoc_net_filtered$mean_strength###chnge this as needed
 E(g)$Color <- ifelse(assoc_net_filtered$direction == "negative", "blue", "red")
 E(g)$Value2 <- transformed_strength$mean_strength_dir #
-# Obtain NMDS layout coordinates
-#nmds_layout <- layout_with_mds(g, dim = 2) #this isnt great on many
+
 
 # Convert the igraph object to a ggplot object with NMDS layout
 gg <- ggnetwork(g)
@@ -213,27 +216,49 @@ mantel_result <- vegan::mantel(dist_matrix1, dist_matrix2, method = "pearson")
 mantel_result #not bad for a first attempt 0.52 and significant
 
 ###################################################
-## Inffering ecological associations (example)
-# based on Pearson correlation
+## Evaluating evaluating co-occurrence network performance
+#using Mantel tests for other co-occurence model types
+
+# based on Pearson correlation. Doesnt work
 network_pred_pea <- abs(cor(t(data_relative)))
 # based on Pearson partial correlation
 network_pred_ppea <- abs(pcor(t(data_relative))$estimate)
+str(network_pred_pea )
 # based on SparCC
-# Note that in this study we used SparCC python module,
-# not the follwing R wrapper function (see Methods section in the main text).
-# (But, similar results are obtained)
+
 network_pred_sparcc <- abs(sparcc(t(data))$Cor)
+
 # based on SPIEC-EASI
 network_pred_spiec <- spiec.easi(t(data),method='mb')
 network_pred_spiec <- as.matrix(getOptMerge(network_pred_spiec))
+dist_matrix3 <- 1 - network_pred_spiec
 
+#on complete 'real' dataset
+dist_matrix1a <- 1-M
 
-## Evaluating evaluating co-occurrence network performance
+#SPIEC-EASI vs real
+mantel_result1 <- vegan::mantel(dist_matrix1a, dist_matrix3, method = "pearson")
+
+pred_pea <- network_pred_pea[lower.tri(network_pred_pea)] # Pearson correlation
+
+str(pred_pea)
+dist_matrix4 <- 1-network_pred_pea
+
+mantel_result2 <- vegan::mantel(dist_matrix1a, dist_matrix4, method = "pearson")
+
+#sparcc
+dist_matrix5 <- 1-network_pred_sparcc
+
+mantel_result3 <- vegan::mantel(dist_matrix1a, dist_matrix5, method = "pearson")
+network_pred_sparcc
 # only use elements in lower triangular matrix
+
+#old code from Takemoto
 real <- network_real[lower.tri(network_real)] # Aij
 pred_pea <- network_pred_pea[lower.tri(network_pred_pea)] # Pearson correlation
 pred_ppea <- network_pred_ppea[lower.tri(network_pred_ppea)] # Pearson partial correlation
 pred_sparcc <- network_pred_sparcc[lower.tri(network_pred_sparcc)] # SparCC
+str(pred_sparcc)
 pred_spiec <- network_pred_spiec[lower.tri(network_pred_spiec)] #SPIEC-EASI
 
 
