f2d8354c86a7815db51cbf970b92818ec6e41f67
Nick Fountain-Jones <33707823+nfj1380@users.noreply.github.com>
2024-07-22 15:33:30 +1000

    other data bug fixes

--- R/Features.R
+++ R/Features.R
@@ -1,12 +1,12 @@
 #' Landscape and host feature (variable) data
 #'
 #' @source Fountain‐Jones, NM, Craft, ME, Funk, WC, et al. Urban landscapes can change virus gene flow and evolution in a fragmentation‐sensitive carnivore. Mol Ecol. 2017; 26: 6487–6498. https://doi.org/10.1111/mec.14375
-#' @format A data frame with 20 columns (features) and 19 rows (sampled bobcat locations):
+#' @format A data frame with 20 columns (features) and 19 rows (sampled bobcat locations). The columns include:
 #' \describe{
 #'   \item{Grassland}{Proportion of grassland in a 5km radius of the sample location.}
 #'   \item{Shrub.Scrub}{Proportion of scrub in a 5km radius of the sample location.}
 #'   \item{Forest}{Proportion of forest in a 5km radius of the sample location.}
-#'   # Add similar entries for other columns if applicable
+#'   \item{...}{Other columns representing additional features, each with a description of the feature they represent.}
 #' }
 #' @examples
 #' \dontrun{
--- R/Responsedata.R
+++ R/Responsedata.R
@@ -1,10 +1,8 @@
 #' Viral single nucleotide polymorphism (SNP) data
 #'
-#' @source Fountain‐Jones, NM, Craft, ME, Funk, WC, et al. (2017). Urban landscapes can change virus gene flow and evolution in a fragmentation‐sensitive carnivore. Molecular Ecology, 26(25), 6487–6498. https://doi.org/10.1111/mec.14375
-#' @format A data frame with 171 columns (each representing a SNP) and 19 rows (each representing a sampled bobcat):
-#' \describe{
-#'   \item{env1}{Presence/absence data from one locus across 19 bobcats from the envelope gene.}
-#' }
+#' @source Fountain‐Jones, NM, Craft, ME, Funk, WC, et al. Urban landscapes can change virus gene flow and evolution in a fragmentation‐sensitive carnivore. Mol Ecol. 2017; 26: 6487–6498. https://doi.org/10.1111/mec.14375
+#' @format A data frame with 171 columns (each column is a SNP) and 19 rows (each row is a sampled bobcat). The columns represent SNPs and the rows represent individual bobcats.
+#' @details The dataset includes presence/absence data from one locus across 19 bobcats from the envelope gene. Each column represents a single SNP, and each row corresponds to a bobcat.
 #' @examples
 #' \dontrun{
 #'   head(Responsedata)
