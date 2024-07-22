687a268cb830e88e22f962d497b84217d6dcf4c2
Nick Fountain-Jones <33707823+nfj1380@users.noreply.github.com>
2024-07-22 15:26:29 +1000

    data file fixes
    formatting issue - should remove warnings

--- R/Responsedata.R
+++ R/Responsedata.R
@@ -1,11 +1,12 @@
-#' Viral single nucleuotide polymorphism (SNP) data.
+#' Viral single nucleotide polymorphism (SNP) data
 #'
-#' @source Fountain‐Jones, NM, Craft, ME, Funk, WC, et al. Urban landscapes can change virus gene flow and evolution in a fragmentation‐sensitive carnivore. Mol Ecol. 2017; 26: 6487– 6498. https://doi.org/10.1111/mec.14375
-#' @format A data frame with 171 columns (each column is a SNP) and 19 rows (each row is a sampled bobcat):
+#' @source Fountain‐Jones, NM, Craft, ME, Funk, WC, et al. (2017). Urban landscapes can change virus gene flow and evolution in a fragmentation‐sensitive carnivore. Molecular Ecology, 26(25), 6487–6498. https://doi.org/10.1111/mec.14375
+#' @format A data frame with 171 columns (each representing a SNP) and 19 rows (each representing a sampled bobcat):
 #' \describe{
-#'  \item{env1}{Presence/absence data from one loci across 19 bobctas from the envelope gene}
+#'   \item{env1}{Presence/absence data from one locus across 19 bobcats from the envelope gene.}
+#' }
 #' @examples
 #' \dontrun{
-#'  Responsedata
+#'   head(Responsedata)
 #' }
 "Responsedata"
--- R/gfData.R
+++ R/gfData.R
@@ -1,10 +1,13 @@
-#' Regression data from Fitzpatrick et al 2014
+#' Regression data from Fitzpatrick et al. 2014
 #'
-#' @source Fitzpatrick et al 2014 Ecology Letters doi: 10.1111/ele.12376
+#' @source Fitzpatrick et al. 2014, Ecology Letters, doi: 10.1111/ele.12376
+#' @format A data frame with the following columns:
 #' \describe{
-#'  \item{gfData} Propotion of individuals in each population with each SNP and bioclim data (bio_1, bio_2, bio_7. bio_10, bio_15, bio_18 as well as spatial MEMs)
+#'   \item{gfData}{Proportion of individuals in each population with each SNP and bioclimatic data 
+#'     (bio_1, bio_2, bio_7, bio_10, bio_15, bio_18) as well as spatial MEMs.}
+#' }
 #' @examples
 #' \dontrun{
-#'  gfdata
+#'   head(gfData)
 #' }
 "gfData"
